6bc47fc565c8d48540584119fb455f4a93c0b8a8
fazerxlo <admin@susek.info>
2025-04-21 23:35:53 +0200

    debug view enhancements

--- src/main.c
+++ src/main.c
@@ -277,184 +277,218 @@ uint32_t wakeups = 0;
 
 void print_debug(void)
 {
-	char buf[100];
+    char buf[120]; // Increased buffer size slightly for longer lines
 
-	uint8_t acc = car_get_acc();
-	uint8_t ign = car_get_ign();
-	uint8_t sel = car_get_selector();
-	struct radar_t radar;
-	car_get_radar(&radar);
-	int8_t angle = 0;
-	car_get_wheel(&angle);
-	uint8_t park_lights = car_get_park_lights();
-	uint8_t near_lights = car_get_near_lights();
-	uint8_t ill = car_get_illum();
+    // --- Retrieve ALL relevant states using getter functions ---
+    enum e_car_t car = car_get_car();
+    enum e_canbox_t cb = conf_get_canbox();
+    uint8_t svin[18]; car_get_vin(svin); // Use uint8_t array for VIN
+    uint32_t odo = car_get_odometer();
 
-	clr_rscreen();
+    uint8_t acc = car_get_acc();
+    uint8_t ign = car_get_ign();
+    uint8_t engine_running = car_get_engine(); // Get engine running status
+    uint32_t rpm = car_get_taho(); // Get RPM
+    uint16_t speed_kmh = car_get_speed(); // Get speed
 
-	char svin[18];
-	car_get_vin((uint8_t *)svin);
+    enum e_selector_t sel_enum = car_get_selector();
+    uint8_t rear_active = get_rear_delay_state();
+    int8_t wheel_angle = 0; car_get_wheel(&wheel_angle); // Assuming -100 to +100
 
-	uint32_t odo = car_get_odometer();
+    uint8_t illum_level = car_get_illum(); // This is likely the raw brightness level
+    uint8_t park_lights = car_get_park_lights();
+    uint8_t near_lights = car_get_near_lights();
 
-	const char * scar = "";
-	enum e_car_t car = car_get_car();
-	switch (car) {
-		case e_car_anymsg:
-			scar = "Any Msg";
-			break;
-		case e_car_lr2_2007my:
-			scar = "LR2 2007MY";
-			break;
-		case e_car_lr2_2013my:
-			scar = "LR2 2013MY";
-			break;
-		case e_car_peugeot_407:
-			scar = "PEUGEOT 407 2008MY";
-			break;
-		case e_car_xc90_2007my:
-			scar = "XC90 2007MY";
-			break;
-		case e_car_skoda_fabia:
-			scar = "SKODA FABIA 2006MY";
-			break;
-		case e_car_q3_2015:
-			scar = "Audi Q3 2015";
-			break;
-		case e_car_toyota_premio_26x:
-			scar = "TOYOTA PREMIO 26x";
-			break;
-		default:
-			break;
-	}
+    struct radar_t radar; car_get_radar(&radar);
 
-	const char * scb = "";
-	enum e_canbox_t cb = conf_get_canbox();
-	switch (cb) {
+    uint8_t fl_door = car_get_door_fl();
+    uint8_t fr_door = car_get_door_fr();
+    uint8_t rl_door = car_get_door_rl();
+    uint8_t rr_door = car_get_door_rr();
+    uint8_t bonnet = car_get_bonnet();
+    uint8_t tailgate = car_get_tailgate();
 
-		case e_cb_raise_vw_pq:
-			scb = "Raise VW(PQ)";
-			break;
-		case e_cb_raise_vw_mqb:
-			scb = "Raise VW(MQB)";
-			break;
-		case e_cb_od_bmw_nbt_evo:
-			scb = "Oudi BMW(NBT)";
-			break;
-		case e_cb_hiworld_vw_mqb:
-			scb = "HiWorld VW(MQB)";
-			break;
-		case e_cb_hiworld_psa_pf2:
-			scb = "Hiworld PSA(PF2)";
-			break;
+    uint8_t park_brake = car_get_park_break();
+    uint8_t low_washer = car_get_low_washer();
+    uint8_t ds_belt = car_get_ds_belt(); // 1 = Unfastened
 
-		default:
-			break;
-	}
+    uint32_t voltage_mv = car_get_voltage(); // Voltage likely in mV or scaled
+    uint8_t low_voltage = car_get_low_voltage();
+    int16_t temp_out = car_get_temp();
+    uint8_t fuel_lvl_pct = car_get_fuel_level();
+    uint8_t low_fuel = car_get_low_fuel_level();
+    int16_t temp_coolant = car_get_engine_temp();
+    int16_t temp_oil = car_get_oil_temp();
 
-	snprintf(buf, sizeof(buf), "Main window\r\n");
-	hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+    // Trip Data
+    uint16_t inst_cons_raw = car_get_inst_consumption_raw();
+    uint16_t range_km = car_get_range_km();
+    uint16_t avg_speed1 = car_get_avg_speed1();
+    uint16_t avg_cons1_raw = car_get_avg_consumption1_raw();
+    uint32_t trip_dist1 = car_get_trip_distance1(); // Note: 32-bit
+    uint16_t avg_speed2 = car_get_avg_speed2();
+    uint16_t avg_cons2_raw = car_get_avg_consumption2_raw();
+    uint32_t trip_dist2 = car_get_trip_distance2(); // Note: 32-bit
 
-	print_line();
+    // Climate Data (Optional section)
+    uint8_t ac_on = car_get_air_ac();
+    uint8_t ac_max = car_get_air_ac_max();
+    uint8_t recirc = car_get_air_recycling();
+    uint8_t dual = car_get_air_dual();
+    uint8_t rear_defrost = car_get_air_rear();
+    uint8_t aqs = car_get_air_aqs();
+    uint8_t fan_speed = car_get_air_fanspeed();
+    uint8_t air_wind = car_get_air_wind();
+    uint8_t air_mid = car_get_air_middle();
+    uint8_t air_floor = car_get_air_floor();
+    uint8_t temp_l_raw = car_get_air_l_temp(); // Raw value from CAN
+    uint8_t temp_r_raw = car_get_air_r_temp(); // Raw value from CAN
 
-	snprintf(buf, sizeof(buf), "Configuration(%d)\r\n", conf_get_idx());
-	hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
 
-	print_line();
+    // --- Clear Screen & Print Header ---
+    clr_rscreen(); // Clear screen from cursor down
 
-	snprintf(buf, sizeof(buf), "Car: %s Vin: %s  %lu km        \r\n", scar, svin, odo);
-	hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+    snprintf(buf, sizeof(buf), "Main window\r\n");
+    hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+    print_line();
 
-	snprintf(buf, sizeof(buf), "CanBox: %s          \r\n", scb);
-	hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+    // --- Configuration Section ---
+    snprintf(buf, sizeof(buf), "Configuration(%"PRIu16")\r\n", conf_get_idx()); // Use PRIu16 for uint16_t
+    hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+    print_line();
 
-	snprintf(buf, sizeof(buf), "Conf: Illum:%d Rear Delay:%d          \r\n", conf_get_illum(), conf_get_rear_delay());
-	hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+    const char * scar = "";
+    switch (car) { /* ... (keep existing car name logic) ... */
+        case e_car_anymsg: scar = "Any Msg"; break;
+        case e_car_lr2_2007my: scar = "LR2 2007MY"; break;
+        case e_car_lr2_2013my: scar = "LR2 2013MY"; break;
+        case e_car_peugeot_407: scar = "PEUGEOT 407 2008MY"; break;
+        case e_car_xc90_2007my: scar = "XC90 2007MY"; break;
+        case e_car_skoda_fabia: scar = "SKODA FABIA 2006MY"; break;
+        case e_car_q3_2015: scar = "Audi Q3 2015"; break;
+        case e_car_toyota_premio_26x: scar = "TOYOTA PREMIO 26x"; break;
+        default: scar = "Unknown"; break;
+    }
 
-	snprintf(buf, sizeof(buf), "State\r\n");
-	hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+    const char * scb = "";
+    switch (cb) { /* ... (keep existing canbox name logic) ... */
+        case e_cb_raise_vw_pq: scb = "Raise VW(PQ)"; break;
+        case e_cb_raise_vw_mqb: scb = "Raise VW(MQB)"; break;
+        case e_cb_od_bmw_nbt_evo: scb = "Oudi BMW(NBT)"; break;
+        case e_cb_hiworld_vw_mqb: scb = "HiWorld VW(MQB)"; break;
+        case e_cb_hiworld_psa_pf2: scb = "Hiworld PSA(PF2)"; break;
+        case e_cb_raise_psa_rcz: scb = "Raise RZC PSA"; break; // Added RZC
+        default: scb = "Unknown"; break;
+    }
 
-	print_line();
+    // Null-terminate VIN just in case car_get_vin didn't
+    svin[sizeof(svin)-1] = '\0';
+    snprintf(buf, sizeof(buf), "Car: %s Vin: %s Odo:%"PRIu32"km\r\n", scar, svin, odo);
+    hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
 
-	snprintf(buf, sizeof(buf), "Uptime: %.5" PRIu32 ".%.3" PRIu32 " Wakeups: %" PRIu32 "     \r\n", timer.sec, timer.msec, wakeups);
-	hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+    snprintf(buf, sizeof(buf), "CanBox: %s\r\n", scb);
+    hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
 
-	const char * ssel = "x";
-	switch (sel) {
-		case e_selector_p:
-			ssel = "P";
-			break;
-		case e_selector_r:
-			ssel = "R";
-			break;
-		case e_selector_n:
-			ssel = "N";
-			break;
-		case e_selector_d:
-			ssel = "D";
-			break;
-		case e_selector_m:
-			ssel = "M";
-			break;
-		case e_selector_m_p:
-			ssel = "M+";
-			break;
-		case e_selector_m_m:
-			ssel = "M-";
-			break;
-		case e_selector_s:
-			ssel = "S";
-			break;
-		default:
-			break;
-	}
+    snprintf(buf, sizeof(buf), "Conf: Illum:%d Rear Delay:%d\r\n", conf_get_illum(), conf_get_rear_delay());
+    hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
 
-	snprintf(buf, sizeof(buf), "Acc:%d Ign:%d Selector:%s R:%d Wheel:%d  \r\n", acc, ign, ssel, get_rear_delay_state(), angle);
-	hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+    // --- State Section ---
+    snprintf(buf, sizeof(buf), "State\r\n");
+    hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+    print_line();
 
-	snprintf(buf, sizeof(buf), "Illum:%d ParkLights:%d NearLights:%d     \r\n", ill, park_lights, near_lights);
-	hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+    snprintf(buf, sizeof(buf), "Sys : Uptime:%.5"PRIu32".%.3"PRIu32" Wakeups:%"PRIu32"\r\n", timer.sec, timer.msec % 1000, wakeups); // Corrected msec display
+    hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
 
-	snprintf(buf, sizeof(buf), "Park:%02x Front:%02x %02x %02x %02x Rear:%02x %02x %02x %02x\r\n",
-		radar.state,
-		radar.fl, radar.flm, radar.frm, radar.fr,
-		radar.rl, radar.rlm, radar.rrm, radar.rr);
-	hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+    const char * ssel = "x"; // Selector string
+    switch (sel_enum) { /* ... (keep existing selector logic) ... */
+        case e_selector_p: ssel = "P"; break;
+        case e_selector_r: ssel = "R"; break;
+        case e_selector_n: ssel = "N"; break;
+        case e_selector_d: ssel = "D"; break;
+        case e_selector_m: ssel = "M"; break;
+        case e_selector_m_p: ssel = "M+"; break;
+        case e_selector_m_m: ssel = "M-"; break;
+        case e_selector_s: ssel = "S"; break;
+        default: ssel = "?"; break; // Handle unknown state
+    }
 
-	//uint32_t rx_ovr = hw_usart_get_rx_overflow(hw_usart_get());
-	//uint32_t tx_ovr = hw_usart_get_tx_overflow(hw_usart_get());
-	//uint32_t rx_cnt = hw_usart_get_rx(hw_usart_get());
-	//uint32_t tx_cnt = hw_usart_get_tx(hw_usart_get());
-	//snprintf(buf, sizeof(buf), "Usart: RX:%" PRIu32 " TX:%" PRIu32 " RXOver:%" PRIu32 " TXOver:%" PRIu32 " On:%" PRIu32 " Isr:%" PRIu32 "\r\n", rx_cnt, tx_cnt, rx_ovr, tx_ovr, debug_on_cnt, usart_isr_cnt);
-	//hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+    snprintf(buf, sizeof(buf), "Core: ACC:%d IGN:%d ENG:%d Gear:%s REAR:%d RPM:%"PRIu32"\r\n",
+             acc, ign, engine_running, ssel, rear_active, rpm);
+    hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
 
-	uint8_t n1 = hw_can_get_msg_nums(hw_can_get_mscan());
-	uint32_t n2 = hw_can_get_pack_nums(hw_can_get_mscan());
-	snprintf(buf, sizeof(buf), "Can: IDs:%" PRIu8 " Msgs:%" PRIu32 " Irqs:%" PRIu32 "     \r\n", n1, n2, can_isr_cnt);
-	hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+    snprintf(buf, sizeof(buf), "Drv : Speed:%u km/h Wheel:%d%%\r\n", speed_kmh, wheel_angle); // Added speed
+    hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
 
-	uint8_t msgs_num = hw_can_get_msg_nums(hw_can_get_mscan());
-	if (msgs_num && msg_idx < msgs_num) {
+    snprintf(buf, sizeof(buf), "Lght: Park:%d Near:%d Illum:%d\r\n", park_lights, near_lights, illum_level);
+    hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
 
-		struct msg_can_t msg;
-		if (hw_can_get_msg(hw_can_get_mscan(), &msg, msg_idx)) {
+    snprintf(buf, sizeof(buf), "Door: FL:%d FR:%d RL:%d RR:%d Bonnet:%d Tailgate:%d\r\n",
+             fl_door, fr_door, rl_door, rr_door, bonnet, tailgate);
+    hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
 
-			snprintf(buf, sizeof(buf), "Can%d/%d:  %08x:%02x.%02x.%02x.%02x.%02x.%02x.%02x.%02x     \r\n",
-					msg_idx + 1, msgs_num, (int)msg.id, msg.data[0], msg.data[1], msg.data[2], msg.data[3], msg.data[4], msg.data[5], msg.data[6], msg.data[7]);
-			hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
-		}
-	}
+    snprintf(buf, sizeof(buf), "Stat: PB:%d Belt:%d(1=Open) Washer:%d(1=Low)\r\n",
+             park_brake, ds_belt, low_washer);
+    hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
 
-	print_line();
+    snprintf(buf, sizeof(buf), "Temp: Out:%d C Coolant:%d C Oil:%d C\r\n",
+             temp_out, temp_coolant, temp_oil);
+    hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
 
-	snprintf(buf, sizeof(buf), "Ctrl keys: o - exit; m - shift msgs\r\n");
-	hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
-	snprintf(buf, sizeof(buf), "i/I - set Illum; d/D - set rear Delay\r\n");
-	hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
-	snprintf(buf, sizeof(buf), "c - set Car; b - set Canbox\r\n");
-	hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
-	snprintf(buf, sizeof(buf), "s - save conf; S - to Sniffer window\r\n");
-	hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+    snprintf(buf, sizeof(buf), "Fuel: %3u%%(Low:%d) Volt:%.2fV(Low:%d)\r\n",
+             fuel_lvl_pct, low_fuel, (float)voltage_mv / 100.0f, low_voltage); // Assuming voltage is mV*10
+    hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+
+    // Trip Computer Data (Using raw values - scaling interpretation depends on protocol)
+    snprintf(buf, sizeof(buf), "Trip0: InstCons:%5u Range:%4u km\r\n", inst_cons_raw, range_km);
+    hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+    snprintf(buf, sizeof(buf), "Trip1: AvgC:%5u AvgS:%3u Dst:%5"PRIu32"\r\n", avg_cons1_raw, avg_speed1, trip_dist1); // Use PRIu32 for distance
+    hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+    snprintf(buf, sizeof(buf), "Trip2: AvgC:%5u AvgS:%3u Dst:%5"PRIu32"\r\n", avg_cons2_raw, avg_speed2, trip_dist2); // Use PRIu32 for distance
+    hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+
+
+    // --- Climate Section (Optional - Add if useful) ---
+    snprintf(buf, sizeof(buf), "AC  : On:%d Max:%d Rec:%d Dual:%d Rear:%d AQS:%d Fan:%d\r\n",
+             ac_on, ac_max, recirc, dual, rear_defrost, aqs, fan_speed);
+    hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+    snprintf(buf, sizeof(buf), "Air : Wind:%d Mid:%d Floor:%d | Temp L:%02X R:%02X (Raw)\r\n",
+             air_wind, air_mid, air_floor, temp_l_raw, temp_r_raw);
+    hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+    // Add seat heating if needed: LSeat:%d RSeat:%d
+
+    // --- Radar Section ---
+    snprintf(buf, sizeof(buf), "Radar St:%X | F:%d %d %d %d | R:%d %d %d %d (0-7)\r\n",
+             radar.state,
+             radar.fl, radar.flm, radar.frm, radar.fr,
+             radar.rl, radar.rlm, radar.rrm, radar.rr);
+    hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+
+    // --- CAN Stats/Message Section ---
+    uint8_t n1 = hw_can_get_msg_nums(hw_can_get_mscan());
+    uint32_t n2 = hw_can_get_pack_nums(hw_can_get_mscan());
+    snprintf(buf, sizeof(buf), "CAN : IDs:%"PRIu8" Msgs:%"PRIu32" Irqs:%"PRIu32"\r\n", n1, n2, can_isr_cnt); // Use PRIu8/PRIu32
+    hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+
+    uint8_t msgs_num = hw_can_get_msg_nums(hw_can_get_mscan());
+    if (msgs_num > 0 && msg_idx < msgs_num) {
+        struct msg_can_t msg;
+        if (hw_can_get_msg(hw_can_get_mscan(), &msg, msg_idx)) {
+            snprintf(buf, sizeof(buf), "Msg%d/%d:%03X#%02X%02X%02X%02X%02X%02X%02X%02X\r\n", // Compact hex format
+                     msg_idx + 1, msgs_num, (unsigned int)msg.id, // Cast id to unsigned int for %X
+                     msg.data[0], msg.data[1], msg.data[2], msg.data[3],
+                     msg.data[4], msg.data[5], msg.data[6], msg.data[7]);
+            hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+        }
+    } else if (msgs_num == 0) {
+         snprintf(buf, sizeof(buf), "Msg -/-: No messages received yet.\r\n");
+         hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
+    }
+
+    // --- Footer / Commands ---
+    print_line();
+    snprintf(buf, sizeof(buf), "Keys: o:Exit m:NxtMsg s:Save S:Sniff c:Car b:Box i/I:Illum d/D:Delay\r\n");
+    hw_usart_write(hw_usart_get(), (uint8_t *)buf, strlen(buf));
 }
 
 static void gpio_process(void)
